fd7e3081c5a0d1025dce428cd29604f72a450b3a
Akylbek Askharov <androidepta@gmail.com>
2026-07-10 19:26:42 +0500

    add signal body_exited to maze -> level1

--- level.gd
+++ level.gd
@@ -3,9 +3,13 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Hello, World!")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_maze_body_exited(body: Node2D) -> void:
+	get_tree().reload_current_scene()
